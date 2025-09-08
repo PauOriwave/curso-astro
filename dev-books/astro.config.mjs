c34b7b9c8a8cefb0a4142e9cf9420610be9a08db
Pau A <Heiwabeats@gmail.com>
2025-09-08 07:14:22 +0200

    transitions added

--- dev-books/astro.config.mjs
+++ dev-books/astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    transitions: true, // ✅ Activa el soporte para ClientRouter
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
